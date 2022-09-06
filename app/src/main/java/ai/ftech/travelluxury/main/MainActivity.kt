c4fea58068e90d863ba1bb09024989f1c6398e46
Đoàn Thanh Sơn <thanhsondoan02@gmail.com>
2022-09-06 15:29:13 +0700

    bottom navigation bar UI

--- app/src/main/java/ai/ftech/travelluxury/main/MainActivity.kt
+++ app/src/main/java/ai/ftech/travelluxury/main/MainActivity.kt
@@ -7,6 +7,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.big_features_item)
+        setContentView(R.layout.main_activity)
     }
 }
