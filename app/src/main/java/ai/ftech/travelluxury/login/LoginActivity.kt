924b3d139f9205a99d79e9b2573882cf55b8d149
Đoàn Thanh Sơn <thanhsondoan02@gmail.com>
2022-09-07 10:23:10 +0700

    global val

--- app/src/main/java/ai/ftech/travelluxury/login/LoginActivity.kt
+++ app/src/main/java/ai/ftech/travelluxury/login/LoginActivity.kt
@@ -11,53 +11,68 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener, LoginContract.View {
 
     private val presenter = LoginPresenter(this)
+    private lateinit var actionBar: ActionBarView
+    private lateinit var footer: FooterView
+    private lateinit var buttonLogin: Button
+    private lateinit var footerRightTextView: TextView
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var emailErrorTextView: TextView
+    private lateinit var passwordErrorTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
-        setUpTextInLayout()
+        initView()
 
-        findViewById<Button>(R.id.button_login).setOnClickListener(this)
-        findViewById<TextView>(R.id.footer_right_text_view).setOnClickListener(this)
+        buttonLogin.setOnClickListener(this)
+        footerRightTextView.setOnClickListener(this)
 
         // email change -> remove error text
         resetErrorTextOnInputTextChange(
-            findViewById(R.id.email_edit_text),
-            findViewById(R.id.email_error_text_view)
+            emailEditText,
+            emailErrorTextView
         )
 
         // password change -> remove error text
         resetErrorTextOnInputTextChange(
-            findViewById(R.id.password_edit_text),
-            findViewById(R.id.password_error_text_view)
+            passwordEditText,
+            passwordErrorTextView
         )
     }
 
-    private fun setUpTextInLayout() {
-        // action bar
-        findViewById<ActionBarView>(R.id.action_bar).setTitle("Login")
 
-        // footer
-        findViewById<FooterView>(R.id.footer).setLeftTextView("Don't have a Travel Luxury account?")
-        findViewById<FooterView>(R.id.footer).setRightTextView("Register now")
+    private fun initView() {
+        actionBar = findViewById(R.id.action_bar)
+        footer = findViewById(R.id.footer)
+        buttonLogin = findViewById(R.id.button_login)
+        footerRightTextView = findViewById(R.id.footer_right_text_view)
+        emailEditText = findViewById(R.id.email_edit_text)
+        passwordEditText = findViewById(R.id.password_edit_text)
+        emailErrorTextView = findViewById(R.id.email_error_text_view)
+        passwordErrorTextView = findViewById(R.id.password_error_text_view)
 
-        // error text
-        findViewById<TextView>(R.id.email_error_text_view).text = ""
-        findViewById<TextView>(R.id.password_error_text_view).text = ""
+        // init text
+        actionBar.setTitle("Login")
+        footer.setLeftTextView("Don't have a Travel Luxury account?")
+        footer.setRightTextView("Register now")
+        emailErrorTextView.text = ""
+        passwordErrorTextView.text = ""
     }
 
     override fun onClick(p0: View?) {
         when (p0?.id) {
             R.id.button_login -> {
-                val userName = findViewById<TextView>(R.id.email_edit_text).text.toString()
-                val password = findViewById<TextView>(R.id.password_edit_text).text.toString()
+                val userName = emailEditText.text.toString()
+                val password = passwordEditText.text.toString()
                 presenter.handleLogin(userName, password)
             }
             R.id.footer_right_text_view -> {
@@ -69,16 +84,16 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener, LoginContract.V
     override fun onLoginResult(state: LOGIN_STATE, message: String) {
         when (state) {
             LOGIN_STATE.EMPTY_EMAIL_FIELD -> {
-                findViewById<TextView>(R.id.email_error_text_view).text = message
+                emailErrorTextView.text = message
             }
             LOGIN_STATE.INVALID_EMAIL_FORMAT -> {
-                findViewById<TextView>(R.id.email_error_text_view).text = message
+                emailErrorTextView.text = message
             }
             LOGIN_STATE.EMPTY_PASSWORD_FIELD -> {
-                findViewById<TextView>(R.id.password_error_text_view).text = message
+                passwordErrorTextView.text = message
             }
             LOGIN_STATE.INVALID_PASSWORD_FORMAT -> {
-                findViewById<TextView>(R.id.password_error_text_view).text = message
+                passwordErrorTextView.text = message
             }
             LOGIN_STATE.WRONG_EMAIL_OR_PASSWORD -> {
                 Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
--- app/src/main/java/ai/ftech/travelluxury/register/RegisterActivity.kt
+++ app/src/main/java/ai/ftech/travelluxury/register/RegisterActivity.kt
@@ -17,11 +17,9 @@ import android.widget.Toast
 
 fun resetErrorTextOnInputTextChange(inputText: EditText, errorText: TextView) {
     inputText.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(s: Editable) {
-        }
+        override fun afterTextChanged(s: Editable) {}
 
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-        }
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             errorText.text = ""
@@ -30,41 +28,52 @@ fun resetErrorTextOnInputTextChange(inputText: EditText, errorText: TextView) {
 }
 
 class RegisterActivity : AppCompatActivity(), RegisterContract.View, View.OnClickListener {
+
+    private lateinit var buttonRegister: Button
+    private lateinit var footerRightTextView: TextView
+    private lateinit var emailEditText: EditText
+    private lateinit var emailErrorTextView: TextView
+    private lateinit var actionBar: ActionBarView
+    private lateinit var footer: FooterView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
 
-        setUpTextInLayout()
+        initView()
 
-        findViewById<Button>(R.id.button_register).setOnClickListener(this)
-        findViewById<TextView>(R.id.footer_right_text_view).setOnClickListener(this)
+        buttonRegister.setOnClickListener(this)
+        footerRightTextView.setOnClickListener(this)
 
         // email change -> remove error text
         resetErrorTextOnInputTextChange(
-            findViewById(R.id.email_edit_text),
-            findViewById(R.id.email_error_text_view)
+            emailEditText,
+            emailErrorTextView
         )
     }
 
-    private fun setUpTextInLayout() {
-        // action bar
-        findViewById<ActionBarView>(R.id.action_bar).setTitle("Become a Travel Luxury Member")
+    private fun initView() {
+        buttonRegister = findViewById(R.id.button_register)
+        footerRightTextView = findViewById(R.id.footer_right_text_view)
+        emailEditText = findViewById(R.id.email_edit_text)
+        emailErrorTextView = findViewById(R.id.email_error_text_view)
+        actionBar = findViewById(R.id.action_bar)
+        footer = findViewById(R.id.footer)
 
-        // footer
-        findViewById<FooterView>(R.id.footer).setLeftTextView("Already have a Travel Luxury account?")
-        findViewById<FooterView>(R.id.footer).setRightTextView("Log in now")
-
-        // error text
-        findViewById<TextView>(R.id.email_error_text_view).text = ""
+        // init text
+        actionBar.setTitle("Become a Travel Luxury Member")
+        footer.setLeftTextView("Already have a Travel Luxury account?")
+        footer.setRightTextView("Log in now")
+        emailErrorTextView.text = ""
     }
 
     override fun onRegisterResult(state: REGISTER_STATE, message: String) {
         when (state) {
             REGISTER_STATE.EMPTY_EMAIL_FIELD -> {
-                findViewById<TextView>(R.id.email_error_text_view).text = message
+                emailErrorTextView.text = message
             }
             REGISTER_STATE.INVALID_EMAIL_FORMAT -> {
-                findViewById<TextView>(R.id.email_error_text_view).text = message
+                emailErrorTextView.text = message
             }
             REGISTER_STATE.CONFIRM_EMAIL -> {
                 Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -78,7 +87,7 @@ class RegisterActivity : AppCompatActivity(), RegisterContract.View, View.OnClic
                 startActivity(Intent(this, LoginActivity::class.java))
             }
             R.id.button_register -> {
-                val email = findViewById<TextView>(R.id.email_edit_text).text.toString()
+                val email = emailEditText.text.toString()
                 val presenter = RegisterPresenter(this)
                 presenter.handleRegister(email)
             }
