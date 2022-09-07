9c76c323c5295561524da2f956df883888b22fd1
Đoàn Thanh Sơn <thanhsondoan02@gmail.com>
2022-09-07 09:49:11 +0700

    using when instead of if else

--- app/src/main/java/ai/ftech/travelluxury/login/LoginPresenter.kt
+++ app/src/main/java/ai/ftech/travelluxury/login/LoginPresenter.kt
@@ -5,18 +5,31 @@ import android.util.Patterns
 class LoginPresenter(private val view: LoginContract.View) : LoginContract.Presenter {
 
     override fun handleLogin(email: String, password: String) {
-        if (email.isEmpty()) {
-            view.onLoginResult(LOGIN_STATE.EMPTY_EMAIL_FIELD, "Email is required")
-        } else if (!isValidEmail(email)) {
-            view.onLoginResult(LOGIN_STATE.INVALID_EMAIL_FORMAT, "Invalid email format")
-        } else if (password.isEmpty()) {
-            view.onLoginResult(LOGIN_STATE.EMPTY_PASSWORD_FIELD, "Password is required")
-        } else if (!isValidPassword(password)) {
-            view.onLoginResult(LOGIN_STATE.INVALID_PASSWORD_FORMAT, "Invalid password format")
-        } else if (isTrueAccount(email, password)) {
-            view.onLoginResult(LOGIN_STATE.SUCCESS, "Awesome! You have been logged in successfully")
-        } else {
-            view.onLoginResult(LOGIN_STATE.WRONG_EMAIL_OR_PASSWORD, "Your email and password combination is incorrect. Please try again.")
+        when {
+            email.isEmpty() -> view.onLoginResult(
+                LOGIN_STATE.EMPTY_EMAIL_FIELD,
+                "Email is required"
+            )
+            !isValidEmail(email) -> view.onLoginResult(
+                LOGIN_STATE.INVALID_EMAIL_FORMAT,
+                "Invalid email format"
+            )
+            password.isEmpty() -> view.onLoginResult(
+                LOGIN_STATE.EMPTY_PASSWORD_FIELD,
+                "Password is required"
+            )
+            !isValidPassword(password) -> view.onLoginResult(
+                LOGIN_STATE.INVALID_PASSWORD_FORMAT,
+                "Minimum length 6 character(s)"
+            )
+            isTrueAccount(email, password) -> view.onLoginResult(
+                LOGIN_STATE.SUCCESS,
+                "Awesome! You have been logged in successfully"
+            )
+            else -> view.onLoginResult(
+                LOGIN_STATE.WRONG_EMAIL_OR_PASSWORD,
+                "Your email and password combination is incorrect. Please try again."
+            )
         }
     }
 
