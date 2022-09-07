b54463d8ca95fac9be355429493ca75862a5d96b
Đoàn Thanh Sơn <thanhsondoan02@gmail.com>
2022-09-07 15:42:08 +0700

    navigator with fragment

--- app/src/main/java/ai/ftech/travelluxury/main/MainActivity.kt
+++ app/src/main/java/ai/ftech/travelluxury/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package ai.ftech.travelluxury.main
 
 import ai.ftech.travelluxury.R
+import ai.ftech.travelluxury.main.fragment.*
 import ai.ftech.travelluxury.message.MessageActivity
 import ai.ftech.travelluxury.notification.NotificationActivity
 import ai.ftech.travelluxury.search.SearchActivity
@@ -8,14 +9,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var rlSearchBox: RelativeLayout
     private lateinit var ivNotification: ImageView
     private lateinit var ivMessage: ImageView
+    private lateinit var flContent: FrameLayout
+    private lateinit var bnvNavigator: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +29,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         initView()
         setOnCLickListener()
+
+        loadFragment(HomeFragment())
+
+        setNavigatorListener()
     }
 
     override fun onClick(p0: View?) {
@@ -37,13 +47,48 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         rlSearchBox = findViewById(R.id.rlMainSearchBox)
         ivNotification = findViewById(R.id.ivMainNotification)
         ivMessage = findViewById(R.id.ivMainMessage)
-
+        flContent = findViewById(R.id.flMainContent)
+        bnvNavigator = findViewById(R.id.bnvMainNavigator)
     }
 
     private fun setOnCLickListener() {
         rlSearchBox.setOnClickListener(this)
         ivNotification.setOnClickListener(this)
         ivMessage.setOnClickListener(this)
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.flMainContent, fragment)
+            .commit()
+    }
+
+    private fun setNavigatorListener() {
+        bnvNavigator.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.itmBottomNavigationHome -> {
+                    loadFragment(HomeFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.itmBottomNavigationExplore -> {
+                    loadFragment(ExploreFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.itmBottomNavigationMyBooking -> {
+                    loadFragment(MyBookingFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.itmBottomNavigationSaved -> {
+                    loadFragment(SavedFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.itmBottomNavigationMyAccount -> {
+                    loadFragment(MyAccountFragment())
+                    return@setOnItemSelectedListener true
+                }
+                else -> false
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/ai/ftech/travelluxury/main/fragment/HomeFragment.kt
@@ -0,0 +1,20 @@
+package ai.ftech.travelluxury.main.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ai.ftech.travelluxury.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+
+}
