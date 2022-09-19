a49ca5bc3c8209eef910f15f9994f862e09f48a3
Đoàn Thanh Sơn <thanhsondoan02@gmail.com>
2022-09-19 18:14:06 +0700

    resolve find view 2

--- app/src/main/java/ai/ftech/travelluxury/hoteldetail/FacilitiesAdapter.kt
+++ app/src/main/java/ai/ftech/travelluxury/hoteldetail/FacilitiesAdapter.kt
@@ -2,7 +2,9 @@ package ai.ftech.travelluxury.hoteldetail
 
 import ai.ftech.travelluxury.R
 import ai.ftech.travelluxury.data.HotelFacilitiesHandler
+import ai.ftech.travelluxury.data.TAG
 import ai.ftech.travelluxury.model.hoteldetail.HotelDetail.Companion.HOTEL_DETAIL
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -20,16 +22,19 @@ class FacilitiesAdapter : RecyclerView.Adapter<FacilitiesAdapter.FacilityVH>() {
     }
 
     override fun onBindViewHolder(holder: FacilityVH, position: Int) {
-        if (dataList == null) return
-
-        val ivIcon = holder.itemView.findViewById<ImageView>(R.id.ivHotelDetailFacilitiesIcon)
-        val tvTitle = holder.itemView.findViewById<TextView>(R.id.tvHotelDetailFacilitiesName)
-
-        if (handler.isValidType(dataList[position])) {
-            val type = handler.getType(dataList[position])
-            ivIcon.setImageResource(handler.getIcon(type))
-            tvTitle.text = handler.getTitle(type)
+        if (dataList == null) {
+            Log.d(TAG, "onBindViewHolder: dataList is null")
+            return
         }
+
+        if (!handler.isValidType(dataList[position])) {
+            Log.d(TAG, "onBindViewHolder: invalid facility type ${dataList[position]}")
+            return
+        }
+
+        val type = handler.getType(dataList[position])
+        holder.ivIcon.setImageResource(handler.getIcon(type))
+        holder.tvTitle.text = handler.getTitle(type)
     }
 
     override fun getItemCount(): Int {
@@ -37,10 +42,8 @@ class FacilitiesAdapter : RecyclerView.Adapter<FacilitiesAdapter.FacilityVH>() {
         return dataList.size
     }
 
-    class FacilityVH(itemView: View) : RecyclerView.ViewHolder(itemView)
-
-    data class FacilityData(
-        val iconID: Int,
-        val name: String
-    )
+    class FacilityVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivIcon: ImageView = itemView.findViewById(R.id.ivHotelDetailFacilitiesIcon)
+        val tvTitle: TextView = itemView.findViewById(R.id.tvHotelDetailFacilitiesName)
+    }
 }
